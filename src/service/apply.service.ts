6339fceac852f1406d5309bb855be78397619857
Oscar Noel <ocedanoescalante@email.arizona.edu>
2021-09-24 09:28:25 -0500

    Populate mobile phone

--- src/service/apply.service.ts
+++ src/service/apply.service.ts
@@ -6,7 +6,7 @@ import { publishChallengeGenerationRequest } from './sns.service';
 
 export const apply = async (event: APIGatewayProxyEvent) => {
   console.log('Received Candidate Application Request: ', event.queryStringParameters);
-  const { firstName, lastName, email, format, ...extraFields } = event.queryStringParameters;
+  const { firstName, lastName, email, format, phone, ...extraFields } = event.queryStringParameters;
   const { careerId } = event.pathParameters;
   const { resume } = parse(event, true);
 
@@ -16,11 +16,17 @@ export const apply = async (event: APIGatewayProxyEvent) => {
     firstName,
     lastName,
     email,
+    phone,
     format,
   };
 
+  const candidateFields = {
+    ...extraFields,
+    phone,
+  };
+
   const newCandidate = await createWebResponse(careerId, webResponseFields, resume);
-  await populateCandidateFields(restUrl, BhRestToken, newCandidate.id, extraFields as any);
+  await populateCandidateFields(restUrl, BhRestToken, newCandidate.id, candidateFields as any);
   await saveApplicationNote(restUrl, BhRestToken, newCandidate.id, event.queryStringParameters);
 
   await publishChallengeGenerationRequest(newCandidate.id, +careerId);
