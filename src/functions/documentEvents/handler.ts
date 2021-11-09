1401b4739dd0dc7ddb775cedc909b988812a330d
Oscar Noel <ocedanoescalante@email.arizona.edu>
2021-11-09 16:59:22 -0600

    FIx multipart parsing

--- src/functions/documentEvents/handler.ts
+++ src/functions/documentEvents/handler.ts
@@ -1,12 +1,14 @@
 import { middyfy } from '@libs/lambda';
 import { APIGatewayEvent } from 'aws-lambda';
+import { parse } from 'aws-multipart-parser';
 import { processDocumentEvent } from 'src/service/document.service';
 
 const documentEvents = async (event: APIGatewayEvent) => {
   try {
     switch (event.httpMethod) {
       case 'POST': {
-        await processDocumentEvent(JSON.parse((event.body as any).json));
+        const { json } = parse(event, true);
+        await processDocumentEvent(JSON.parse(json as any));
         return 'Hello API Event Received';
       }
     }
