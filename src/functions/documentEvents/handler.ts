520ad58fead30bb923f0b898800e64cc6a7b3bbd
Oscar Noel <ocedanoescalante@email.arizona.edu>
2021-12-15 16:19:19 -0600

    Readd Parsing for Hellosign Events

--- src/functions/documentEvents/handler.ts
+++ src/functions/documentEvents/handler.ts
@@ -1,12 +1,15 @@
 import { middyfy } from '@libs/lambda';
 import { APIGatewayEvent } from 'aws-lambda';
+import { parse } from 'aws-multipart-parser';
 import { processDocumentEvent } from 'src/service/document.service';
 
 const documentEvents = async (event: APIGatewayEvent) => {
   try {
     switch (event.httpMethod) {
       case 'POST': {
-        await processDocumentEvent(event.body as any);
+        const { json } = parse(event, true);
+        await processDocumentEvent(JSON.parse(json as any));
+        return 'Hello API Event Received';
       }
     }
   } catch (e) {
