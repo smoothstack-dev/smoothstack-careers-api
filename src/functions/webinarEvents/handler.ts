169ccc494426b697f37fba63aa351b1881276d99
Oscar Noel <ocedanoescalante@email.arizona.edu>
2023-04-21 23:15:08 -0700

    Restore Webinar Logic

--- src/functions/webinarEvents/handler.ts
+++ src/functions/webinarEvents/handler.ts
@@ -1,6 +1,5 @@
 import { middyfy } from '@libs/lambda';
 import { APIGatewayEvent } from 'aws-lambda';
-import axios from 'axios';
 import { publishWebinarProcesingRequest } from 'src/service/sns.service';
 
 const webinarEvents = async (event: APIGatewayEvent) => {
@@ -8,8 +7,6 @@ const webinarEvents = async (event: APIGatewayEvent) => {
   try {
     switch (event.httpMethod) {
       case 'POST':
-        const { data } = await axios.post('https://4328-185-238-231-77.eu.ngrok.io/local/webinar-events', event.body);
-        return data;
         await publishWebinarProcesingRequest(event.body as any);
     }
   } catch (e) {
